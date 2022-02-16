fb8f2918acfc0eb22a544f8af5a417bcaf8e1a4a
bhayron <bhayronklivilan@gmail.com>
2022-02-16 15:17:38 -0300

    Finish

--- src/main/java/expertostech/tutorialrestapi/controller/StatusController.java
+++ src/main/java/expertostech/tutorialrestapi/controller/StatusController.java
@@ -8,6 +8,6 @@ public class StatusController {
 
     @GetMapping(path = "/api/status")
     public String check(){
-        return "Api is running";
+        return "API online!";
     }
 }
--- src/main/java/expertostech/tutorialrestapi/controller/UserController.java
+++ src/main/java/expertostech/tutorialrestapi/controller/UserController.java
@@ -1,12 +1,26 @@
 package expertostech.tutorialrestapi.controller;
 
+import expertostech.tutorialrestapi.model.UsuarioModel;
+import expertostech.tutorialrestapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    public ResponseEntity consultar(String codigo){
-        return null;
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping(path = "/api/usuario/{codigo}")
+    public ResponseEntity consultar(@PathVariable("codigo") Integer codigo){
+        return repository.findById(codigo)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/api/usuario/salvar")
+    public UsuarioModel salvar(@RequestBody UsuarioModel usuario) {
+        return repository.save(usuario);
     }
 }
