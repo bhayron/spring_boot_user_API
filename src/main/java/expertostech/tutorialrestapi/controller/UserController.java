2075804235b1dd1bd775d0a8c9943bd675a13314
bhayron <bhayronklivilan@gmail.com>
2022-02-16 11:28:00 -0300

    first commit

---
+++ src/main/java/expertostech/tutorialrestapi/controller/UserController.java
@@ -0,0 +1,12 @@
+package expertostech.tutorialrestapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    public ResponseEntity consultar(String codigo){
+        return null;
+    }
+}
